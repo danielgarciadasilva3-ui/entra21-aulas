bbe8034803e86c73412d8e8f17466d1e14382658
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-04-17 19:57:23 -0300

    jorge

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,5 +1,9 @@
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Ola Mundo!");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Jorge");
+        }
     }
 }
